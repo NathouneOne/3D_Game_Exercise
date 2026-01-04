68af6a9fe6517de38bb6df989ddd6c89a8f29619
NathouneOne <nathan.marin.fx@gmail.com>
2026-01-05 00:14:48 +0100

    small adjustements

--- 3d-games-exercise/player/bullet_3d.gd
+++ 3d-games-exercise/player/bullet_3d.gd
@@ -2,13 +2,20 @@ extends Area3D
 
 const SPEED = 40.0
 const RANGE = 250
+const COMPENSATION_X = 0.5
+const COMPENSATION_Y = 0.3
 
 var traveled_distance=0.0
 
+#########################################################
+## DOESNT SHOOT WHILE MOVING BACKWARD WTFFFFF##
 
 func _physics_process(delta: float) -> void:
 	
 	position += -transform.basis.z * SPEED * delta
+	##trajectory angle compensation
+	#position += -transform.basis.x * COMPENSATION_X * delta
+	position += transform.basis.y * COMPENSATION_Y * delta
 	traveled_distance += SPEED * delta
 	
 	if traveled_distance > RANGE:
