c0051f02707da6c9f26828d365f18e164249f3e6
NathouneOne <nathan.marin.fx@gmail.com>
2026-01-04 13:05:03 +0100

    add main movement

--- 3d-games-exercise/player/player.gd
+++ 3d-games-exercise/player/player.gd
@@ -1,9 +1,10 @@
 extends CharacterBody3D
 
-const X_SENSIBLITY = 0.3
-const Y_SENSIBLITY = 0.3
+const CAMERA_X_SENSIBLITY = 0.3
+const CAMERA_Y_SENSIBLITY = 0.3
 const CAMERA_MAX_ROTATION_Y_UP = 70.0
 const CAMERA_MAX_ROTATION_Y_DOWN = -70.0
+const CHARACTER_MAX_SPEED = 7.0
 
 
 
@@ -18,22 +19,23 @@ func _unhandled_input(event: InputEvent) -> void:
 	#######################################################
 	## mouse rotation handler ##
 	if event is InputEventMouseMotion:
-		rotation_degrees.y -= event.relative.x * X_SENSIBLITY
+		rotation_degrees.y -= event.relative.x * CAMERA_X_SENSIBLITY
 		
-		## this solution kinda work but allows to go further 60
+		## this solution kinda work but allows to go further max range
 		#if (%Camera3D.rotation_degrees.x > -60 and event.relative.y > 0) or (%Camera3D.rotation_degrees.x < 60 and event.relative.y < 0):
-		#	%Camera3D.rotation_degrees.x -= event.relative.y * Y_SENSIBLITY
+		#	%Camera3D.rotation_degrees.x -= event.relative.y * CAMERA_Y_SENSIBLITY
 		
-		## Better solution to truncate to 60 ?
-		#if (%Camera3D.rotation_degrees.x - event.relative.y * Y_SENSIBLITY < CAMERA_MAX_ROTATION_Y_UP) and (%Camera3D.rotation_degrees.x - event.relative.y * Y_SENSIBLITY > CAMERA_MAX_ROTATION_Y_DOWN):
-		#	%Camera3D.rotation_degrees.x -= event.relative.y * Y_SENSIBLITY
-		#elif %Camera3D.rotation_degrees.x - event.relative.y * Y_SENSIBLITY >= CAMERA_MAX_ROTATION_Y_UP :
+		## Bit better solution to truncate to max range
+		#if (%Camera3D.rotation_degrees.x - event.relative.y * CAMERA_Y_SENSIBLITY < CAMERA_MAX_ROTATION_Y_UP) 
+		#and (%Camera3D.rotation_degrees.x - event.relative.y * CAMERA_Y_SENSIBLITY > CAMERA_MAX_ROTATION_Y_DOWN):
+		#	%Camera3D.rotation_degrees.x -= event.relative.y * CAMERA_Y_SENSIBLITY
+		#elif %Camera3D.rotation_degrees.x - event.relative.y * CAMERA_Y_SENSIBLITY >= CAMERA_MAX_ROTATION_Y_UP :
 		#	%Camera3D.rotation_degrees.x = CAMERA_MAX_ROTATION_Y_UP
 		#else :
 		#	%Camera3D.rotation_degrees.x = CAMERA_MAX_ROTATION_Y_DOWN
 		
-		## Best solution
-		%Camera3D.rotation_degrees.x -= event.relative.y * Y_SENSIBLITY
+		## Best solution using built-in method
+		%Camera3D.rotation_degrees.x -= event.relative.y * CAMERA_Y_SENSIBLITY
 		%Camera3D.rotation_degrees.x = clamp(%Camera3D.rotation_degrees.x, CAMERA_MAX_ROTATION_Y_DOWN, CAMERA_MAX_ROTATION_Y_UP)
 	
 		print(%Camera3D.rotation_degrees.x)
@@ -44,5 +46,19 @@ func _unhandled_input(event: InputEvent) -> void:
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 	
 	
+func _physics_process(_delta: float) -> void:
+	#######################################################
+	## Player Movement ##
 	
+	var input_direction_2D = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	## convert 2D input direction to 3D
+	var input_direction_3D = Vector3(input_direction_2D.x, 0.0, input_direction_2D.y)
 	
+	## convert to relative coordinate to player rotation
+	var direction = transform.basis * input_direction_3D
+	
+	## applying speed separatly to x and z to avoid cancel jump & fall with a *0
+	velocity.z = direction.z * CHARACTER_MAX_SPEED
+	velocity.x = direction.x * CHARACTER_MAX_SPEED
+	
+	move_and_slide()
