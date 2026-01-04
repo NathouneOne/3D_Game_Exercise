b152d221c63eb75885d2f83cd18a6c038171f21b
NathouneOne <nathan.marin.fx@gmail.com>
2026-01-04 14:10:16 +0100

    clean code + noControll + double jump + reset on fall + sprint

--- 3d-games-exercise/player/player.gd
+++ 3d-games-exercise/player/player.gd
@@ -5,12 +5,16 @@ const CAMERA_Y_SENSIBLITY = 0.3
 const CAMERA_MAX_ROTATION_Y_UP = 70.0
 const CAMERA_MAX_ROTATION_Y_DOWN = -70.0
 const CHARACTER_MAX_SPEED = 7.0
+const CHARACTER_SPRINT_MAX_SPEED = 12.0
 const CHARACTER_ACCELERATION = 20.0
 const CHARACTER_JUMP_SPEED = 10.0
 const GRAVITY_VALUE = 20.0
 
+const FALLING_VALUE_RESET = -10.0
 
 var player_speed =0.0
+var character_max_speed=CHARACTER_MAX_SPEED
+var jump_count=0
 
 
 func _ready() -> void:
@@ -26,20 +30,6 @@ func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
 		rotation_degrees.y -= event.relative.x * CAMERA_X_SENSIBLITY
 		
-		## this solution kinda work but allows to go further max range
-		#if (%Camera3D.rotation_degrees.x > -60 and event.relative.y > 0) or (%Camera3D.rotation_degrees.x < 60 and event.relative.y < 0):
-		#	%Camera3D.rotation_degrees.x -= event.relative.y * CAMERA_Y_SENSIBLITY
-		
-		## Bit better solution to truncate to max range
-		#if (%Camera3D.rotation_degrees.x - event.relative.y * CAMERA_Y_SENSIBLITY < CAMERA_MAX_ROTATION_Y_UP) 
-		#and (%Camera3D.rotation_degrees.x - event.relative.y * CAMERA_Y_SENSIBLITY > CAMERA_MAX_ROTATION_Y_DOWN):
-		#	%Camera3D.rotation_degrees.x -= event.relative.y * CAMERA_Y_SENSIBLITY
-		#elif %Camera3D.rotation_degrees.x - event.relative.y * CAMERA_Y_SENSIBLITY >= CAMERA_MAX_ROTATION_Y_UP :
-		#	%Camera3D.rotation_degrees.x = CAMERA_MAX_ROTATION_Y_UP
-		#else :
-		#	%Camera3D.rotation_degrees.x = CAMERA_MAX_ROTATION_Y_DOWN
-		
-		## Best solution using built-in method
 		%Camera3D.rotation_degrees.x -= event.relative.y * CAMERA_Y_SENSIBLITY
 		%Camera3D.rotation_degrees.x = clamp(%Camera3D.rotation_degrees.x, CAMERA_MAX_ROTATION_Y_DOWN, CAMERA_MAX_ROTATION_Y_UP)
 	
@@ -66,45 +56,60 @@ func _physics_process(delta: float) -> void:
 	#velocity.z = direction.z * CHARACTER_MAX_SPEED
 	#velocity.x = direction.x * CHARACTER_MAX_SPEED
 	
-	##trying to implement acceleration
-	## little bug when decceleration ends, always move the character slightly on the other axes
-	if direction.z != 0:
-		player_speed += CHARACTER_ACCELERATION*delta
-		player_speed=clamp(player_speed, -CHARACTER_MAX_SPEED, CHARACTER_MAX_SPEED)
+	
+	## no control while jumping hihi
+	if is_on_floor():
+		jump_count=0
 		
-		velocity.z =direction.z * player_speed
-		
-	elif velocity.z > 0 :
-		velocity.z -= CHARACTER_ACCELERATION * delta
-		if velocity.z < 0:
-			velocity.z=0
-	elif velocity.z < 0 :
-		velocity.z += CHARACTER_ACCELERATION * delta
-		if velocity.z > 0:
-			velocity.z=0
+		## sprint handler
+		if Input.is_action_pressed("sprint"):
+			character_max_speed=CHARACTER_SPRINT_MAX_SPEED
+		else :
+			character_max_speed=CHARACTER_MAX_SPEED
 			
-	if direction.x != 0:
-		player_speed += CHARACTER_ACCELERATION*delta
-		player_speed=clamp(player_speed, -CHARACTER_MAX_SPEED, CHARACTER_MAX_SPEED)
-		
-		velocity.x =direction.x * player_speed
-		
-	elif velocity.x > 0 :
-		velocity.x -= CHARACTER_ACCELERATION * delta
-		if velocity.x < 0:
-			velocity.x=0
-	elif velocity.x < 0 :
-		velocity.x += CHARACTER_ACCELERATION * delta
-		if velocity.x > 0:
-			velocity.x=0
 			
+		##trying to implement acceleration
+		## little bug when decceleration ends, ofently move the character slightly on the other axes
+		## guess its form the clamp which wouldn't occur at the exact same time on each axes or something like that ? dunno
+		if direction.z != 0:
+			player_speed += CHARACTER_ACCELERATION*delta
+			player_speed=clamp(player_speed, -character_max_speed, character_max_speed)
+			velocity.z =direction.z * player_speed
+			
+		elif velocity.z > 0 :
+			velocity.z -= CHARACTER_ACCELERATION * delta
+			if velocity.z < 0:
+				velocity.z=0
+		elif velocity.z < 0 :
+			velocity.z += CHARACTER_ACCELERATION * delta
+			if velocity.z > 0:
+				velocity.z=0
+				
+		if direction.x != 0:
+			player_speed += CHARACTER_ACCELERATION*delta
+			player_speed=clamp(player_speed, -character_max_speed, character_max_speed)
+			
+			velocity.x =direction.x * player_speed
+			
+		elif velocity.x > 0 :
+			velocity.x -= CHARACTER_ACCELERATION * delta
+			if velocity.x < 0:
+				velocity.x=0
+		elif velocity.x < 0 :
+			velocity.x += CHARACTER_ACCELERATION * delta
+			if velocity.x > 0:
+				velocity.x=0
+			
+		## JUMP 
+	if Input.is_action_just_pressed("jump") and jump_count < 2:
+		velocity.y = CHARACTER_JUMP_SPEED
+		jump_count+=1
 			
 	## Applying gravity
 	velocity.y-= GRAVITY_VALUE * delta
 	
-	## JUMP 
-	if Input.is_action_just_pressed("jump"):
-		velocity.y = CHARACTER_JUMP_SPEED
-	
+	## reset position at start if falling
+	if position.y < FALLING_VALUE_RESET :
+		position=Vector3(0,1,0)
 	
 	move_and_slide()
