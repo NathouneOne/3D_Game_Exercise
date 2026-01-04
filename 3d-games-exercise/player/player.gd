7b7dd1ea82d65bd2de81b4ca87c0a30abffeb5bf
NathouneOne <nathan.marin.fx@gmail.com>
2026-01-04 12:35:46 +0100

    hide cursor

--- 3d-games-exercise/player/player.gd
+++ 3d-games-exercise/player/player.gd
@@ -2,14 +2,21 @@ extends CharacterBody3D
 
 const X_SENSIBLITY = 0.3
 const Y_SENSIBLITY = 0.3
+const CAMERA_MAX_ROTATION_Y_UP = 70.0
+const CAMERA_MAX_ROTATION_Y_DOWN = -70.0
 
+
+
+func _ready() -> void:
+	
+	## Hide cursor
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
 
 func _unhandled_input(event: InputEvent) -> void:
 	
-	
 	#######################################################
-	## mouse handle
+	## mouse rotation handler ##
 	if event is InputEventMouseMotion:
 		rotation_degrees.y -= event.relative.x * X_SENSIBLITY
 		
@@ -18,15 +25,24 @@ func _unhandled_input(event: InputEvent) -> void:
 		#	%Camera3D.rotation_degrees.x -= event.relative.y * Y_SENSIBLITY
 		
 		## Better solution to truncate to 60 ?
-		if (%Camera3D.rotation_degrees.x - event.relative.y * Y_SENSIBLITY < 60) and (%Camera3D.rotation_degrees.x - event.relative.y * Y_SENSIBLITY > -60):
-			%Camera3D.rotation_degrees.x -= event.relative.y * Y_SENSIBLITY
-		elif %Camera3D.rotation_degrees.x - event.relative.y * Y_SENSIBLITY >= 60 :
-			%Camera3D.rotation_degrees.x = 60
-		else :
-			%Camera3D.rotation_degrees.x = -60
-		#print(%Camera3D.rotation_degrees.x)
+		#if (%Camera3D.rotation_degrees.x - event.relative.y * Y_SENSIBLITY < CAMERA_MAX_ROTATION_Y_UP) and (%Camera3D.rotation_degrees.x - event.relative.y * Y_SENSIBLITY > CAMERA_MAX_ROTATION_Y_DOWN):
+		#	%Camera3D.rotation_degrees.x -= event.relative.y * Y_SENSIBLITY
+		#elif %Camera3D.rotation_degrees.x - event.relative.y * Y_SENSIBLITY >= CAMERA_MAX_ROTATION_Y_UP :
+		#	%Camera3D.rotation_degrees.x = CAMERA_MAX_ROTATION_Y_UP
+		#else :
+		#	%Camera3D.rotation_degrees.x = CAMERA_MAX_ROTATION_Y_DOWN
+		
+		## Best solution
+		%Camera3D.rotation_degrees.x -= event.relative.y * Y_SENSIBLITY
+		%Camera3D.rotation_degrees.x = clamp(%Camera3D.rotation_degrees.x, CAMERA_MAX_ROTATION_Y_DOWN, CAMERA_MAX_ROTATION_Y_UP)
+	
+		print(%Camera3D.rotation_degrees.x)
+		
+	#######################################################
+	## Release mouse appearance on esc press ##
+	elif event.is_action_pressed("ui_cancel"):
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 	
 	
 	
-	######################################################
 	
