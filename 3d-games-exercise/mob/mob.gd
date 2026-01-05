0a5127601c4e86375e2554b5cadd8d9bf278fe18
NathouneOne <nathan.marin.fx@gmail.com>
2026-01-05 10:38:28 +0100

    damage & mob kill

--- 3d-games-exercise/mob/mob.gd
+++ 3d-games-exercise/mob/mob.gd
@@ -1,13 +1,30 @@
 extends RigidBody3D
 
 const MOBSPEED = 6.5
+const PLAYER_DAMAGE=1
+
+var health = 5
 
 @onready var bat_model: Node3D = %Bat_model
 @onready var player = $/root/GAME/Player
+@onready var timer: Timer = %Timer
 
 func take_damage():
+	if(health==0):
+		return
+	
 	bat_model.hurt()
-
+	health-=PLAYER_DAMAGE
+	if health==0:
+		set_physics_process(false)
+		gravity_scale=1.0
+		var direction2 = -global_position.direction_to(player.global_position)
+		var up_repulsive_force =  Vector3.UP * randf_range(0.5,1)
+		apply_central_impulse(up_repulsive_force+direction2*15)
+		timer.start()
+		lock_rotation = false
+	
+	
 
 
 func _physics_process(_delta: float) -> void:
@@ -22,3 +39,7 @@ func _physics_process(_delta: float) -> void:
 	#print(bat_model.rotation.x, bat_model.rotation.z)
 	
 	
+
+
+func _on_timer_timeout() -> void:
+	queue_free()
