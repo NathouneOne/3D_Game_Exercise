24501f4ba69cfba3d898d36e2a2e400011716298
NathouneOne <nathan.marin.fx@gmail.com>
2026-01-05 09:45:16 +0100

    mob following player

--- 3d-games-exercise/mob/bat/bat_model.gd
+++ 3d-games-exercise/mob/bat/bat_model.gd
@@ -6,3 +6,4 @@ extends Node3D
 func hurt():
 	animation_tree.set("parameters/OneShot/request", AnimationNodeOneShot.ONE_SHOT_REQUEST_FIRE)
 	
+	
--- 3d-games-exercise/mob/mob.gd
+++ 3d-games-exercise/mob/mob.gd
@@ -1,7 +1,24 @@
 extends RigidBody3D
 
-@onready var bat_model: Node3D = %Bat_model
+const MOBSPEED = 6.5
 
+@onready var bat_model: Node3D = %Bat_model
+@onready var player = $/root/GAME/Player
 
 func take_damage():
 	bat_model.hurt()
+
+
+
+func _physics_process(_delta: float) -> void:
+	
+	var direction = global_position.direction_to(player.global_position)
+	direction.y+=0.5
+	
+	linear_velocity=direction*MOBSPEED
+	bat_model.rotation.y=Vector3.FORWARD.signed_angle_to(direction, Vector3.UP)+PI
+	
+	
+	#print(bat_model.rotation.x, bat_model.rotation.z)
+	
+	
